4e8b92245e31953fc5cbaf896fca590c94cd781e
AngyB2002 <angelaballestero19@gmail.com>
2024-03-26 18:49:12 -0300

    Ejercicio2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TracksManagerController } from './tracks-manager/tracks-manager.controller';
+import { TracksManagerService } from './tracks-manager/tracks-manager.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, TracksManagerController],
+  providers: [AppService, TracksManagerService],
 })
 export class AppModule {}
