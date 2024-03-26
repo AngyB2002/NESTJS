aeead2a7fc5db492edf155af60e1d9d7a56b811d
AngyB2002 <angelaballestero19@gmail.com>
2024-03-26 18:49:12 -0300

    Ejercicio2

---
+++ src/tracks-manager/tracks-manager.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from '@nestjs/common';
+import { TracksManagerService, iTrack} from './tracks-manager.service';
+
+@Controller('/tracks')
+export class TracksManagerController {
+    constructor(private readonly tracksManagerService: TracksManagerService) {}
+
+@Get()
+getTracks(): iTrack[] {
+    return this.tracksManagerService.getTracks();
+  }
+}
