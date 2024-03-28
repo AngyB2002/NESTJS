b1ebd56af798c6bcffda23bc45e8a962ad40546f
AngyB2002 <angelaballestero19@gmail.com>
2024-03-28 12:45:12 -0300

    .service.ts modificado

--- src/tracks-manager/tracks-manager.controller.ts
+++ src/tracks-manager/tracks-manager.controller.ts
@@ -2,11 +2,11 @@ import { Controller, Get } from '@nestjs/common';
 import { TracksManagerService, iTrack} from './tracks-manager.service';
 
 @Controller('/tracks')
-export class TracksManagerController {
-    constructor(private readonly tracksManagerService: TracksManagerService) {}
+export class TracksManagerController{
+  constructor(private readonly tracksManagerService: TracksManagerService) {}
 
 @Get()
-getTracks(): iTrack[] {
-    return this.tracksManagerService.getTracks();
+getTracks(): iTrack[]{
+  return this.tracksManagerService.getTracks();
   }
 }
